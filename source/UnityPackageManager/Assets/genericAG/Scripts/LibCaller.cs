f41773c1c5e5a890d031c4258995afc8ddbd1357
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2018-07-02 11:23:34 +0200

    Call the library and write result in a simple GUI

---
+++ source/UnityPackageManager/Assets/genericAG/Scripts/LibCaller.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityDotNetLibrary;
+using UnityEngine;
+
+namespace genericAG.Scripts
+{
+    [ExecuteInEditMode]
+    public class LibCaller : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI _constantContent;
+        [SerializeField]
+        private TextMeshProUGUI _methodContent;
+
+
+        private void OnEnable()
+        {
+            _constantContent.text = Library.Constant;
+            _methodContent.text = $"10 + 5 = {Library.Add(10, 5)}";
+        }
+    }
+}
