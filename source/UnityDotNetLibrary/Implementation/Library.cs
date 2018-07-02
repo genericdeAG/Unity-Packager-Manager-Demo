f19f1377c3f85026b1bbad483b66549d859f72f0
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2018-07-02 11:16:09 +0200

    Add hello world class

---
+++ source/UnityDotNetLibrary/Implementation/Library.cs
@@ -0,0 +1,12 @@
+﻿namespace UnityDotNetLibrary
+{
+    public static class Library
+    {
+        public const string Constant = "Hello World from an external .Net Library.";
+
+        public static float Add(float a, float b)
+        {
+            return a + b;
+        }
+    }
+}
