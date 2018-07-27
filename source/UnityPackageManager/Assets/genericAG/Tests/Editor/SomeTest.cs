40e4319eb11684cbebb50861da9fe66a64eef8b5
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2018-07-27 13:47:03 +0200

    Add simple local test

---
+++ source/UnityPackageManager/Assets/genericAG/Tests/Editor/SomeTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace genericAG.Tests.Editor
+{
+    public class SomeTests
+    {
+        [Test]
+        public void IsTrueTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
