5b102450732e0c09157c0d220e0bb9af43cbee8f
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2018-07-27 13:46:22 +0200

    Create test in the library and include testables

---
+++ source/UnityDotNetLibrary/Implementation/Tests/Editor/LibraryTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace UnityDotNetLibrary
+{
+    public class LibraryTests
+    {
+        [Test]
+        public void LibraryTestsSimplePasses()
+        {
+            var a = 5.5f;
+            var b = -103f;
+            var actual = Library.Add(a, b);
+            var expected = a + b;
+            Assert.That(actual, Is.EqualTo(expected).Within(float.Epsilon));
+        }
+    }
+}
